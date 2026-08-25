070ab9cea9586d886c8cd45f902d33521e7a1e53
timonwetzl-ops <timon.wetzl@gmail.com>
2026-08-25 14:11:00 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Trage hier deine eigenen Werte ein, die du von Firebase bekommst
+// (Anleitung dazu bekommst du separat erklärt).
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDu_2A9JFtIKGXFqJdnz-wdsU42IUJ6uAw",
+  authDomain: "candybar-a33ca.firebaseapp.com",
+  projectId: "candybar-a33ca",
+  storageBucket: "candybar-a33ca.firebasestorage.app",
+  messagingSenderId: "310456556896",
+  appId: "1:310456556896:web:82edaf6749748bc46d1205"
+};
+
+// Das ist dein Geheim-Code fürs Dashboard.
+// Ändere "1234" auf einen Code, den nur du kennst.
+export const DASHBOARD_CODE = "1234";
